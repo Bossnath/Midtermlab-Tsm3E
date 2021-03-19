16a743e0f94fcc23dfda414f70d1d869407cd51a
Nathaniel Delos reyes <79882284+Bossnath@users.noreply.github.com>
2021-03-19 20:34:16 +0800

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,34 @@
+class bank
+{
+  
+}
+class Test2
+{  
+public static void main(String args[])
+{  
+SECURITYBANK sb=new SECURITYBANK();
+GSAVE gs=new GSAVE(); 
+LANDBANK lb=new LANDBANK();  
+CHINABANK bc=new CHINABANK();  
+UNIONBANK ub=new UNIONBANK();  
+ING g=new ING(); 
+BDO b=new BDO();
+BPI bp=new BPI(); 
+RCBC r=new RCBC();
+CITIBANK cb=new CITIBANK();
+CIMB ci=new CIMB();
+METROBANK mb=new METROBANK();
+System.out.println("SECURITYBANK Rate of Interest: "+sb.getRateOfInterest());
+System.out.println("GSAVE Rate of Interest: "+gs.getRateOfInterest());
+System.out.println("LANDBANK Rate of Interest: "+lb.getRateOfInterest());  
+System.out.println("CHINABANK Rate of Interest: "+bc.getRateOfInterest());  
+System.out.println("UNIONBANK Rate of Interest: "+ub.getRateOfInterest()); 
+System.out.println("ING Rate of Interest: "+g.getRateOfInterest()); 
+System.out.println("BDO Rate of Interest: "+b.getRateOfInterest()); 
+System.out.println("BPI Rate of Interest: "+bp.getRateOfInterest()); 
+System.out.println("RCBC Rate of Interest: "+r.getRateOfInterest()); 
+System.out.println("CITIBANK Rate of Interest: "+cb.getRateOfInterest()); 
+System.out.println("CIMB Rate of Interest: "+ci.getRateOfInterest()); 
+System.out.println("METROBANK Rate of Interest: "+mb.getRateOfInterest()); 
+}  
+}
